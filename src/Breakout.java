3d2894fe2e9f31e1b060424a042d31e3f31fb057
Brian Chipman <namedtuple@gmail.com>
2015-05-15 02:57:11 -0700

    Inital window created.

---
+++ src/Breakout.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/**
+ Created by Brian on 5/15/2015.
+ */
+public class Breakout {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(700,500);
+        frame.setTitle("Breakout!");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(true);
+    }
+
+}
