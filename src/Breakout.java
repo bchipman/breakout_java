6f5929973c2e001e37ec0307df7ff85ab02d0243
Brian Chipman <namedtuple@gmail.com>
2015-05-15 09:12:19 -0700

    Set background color to black, created Paddle class.

--- src/Breakout.java
+++ src/Breakout.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  Created by Brian on 5/15/2015.
@@ -10,8 +11,28 @@ public class Breakout {
         frame.setSize(700,500);
         frame.setTitle("Breakout!");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.BLACK);
         frame.setVisible(true);
         frame.setResizable(true);
+        Paddle box = new Paddle();
+        frame.add(box);
+
     }
+
+
+    public static class Paddle extends JComponent {
+        public void paintComponent(Graphics g) {
+            Graphics2D g2 = (Graphics2D) g;
+            Rectangle box = new Rectangle(100,200,50,150);
+            g2.draw(box);
+        }
+    }
+
+
+
+
+
+
+
 
 }
