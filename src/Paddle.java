cec758bdb0496841dd566b31e506bd891d770eaf
Brian Chipman <namedtuple@gmail.com>
2015-06-01 01:16:26 -0700

    Slightly better handling of paddle-ball collision by incorporating paddle's direction at time of impact with ball.  Collision now works correctly (no teleporting ball) if paddle has only x velocity or only y velocity (test by holding 'h' or 'v', respectively).  However, if paddle has both x and y velocities, ball teleportation through paddle still occurs because two 'if' statements will be satisfied within the 'checkForBallCollisions' method.

--- src/Paddle.java
+++ src/Paddle.java
@@ -3,11 +3,18 @@
  */
 public class Paddle extends Rect {
 
+    private int lastX;
+    private int lastY;
+
     public Paddle(int startX, int startY, int width, int height) {
         super(startX, startY, width, height);
+        lastX = startX;
+        lastY = startY;
     }
 
     public void setLocation(int newX, int newY) {
+        lastX = x;
+        lastY = y;
         super.setLocation(newX, newY);
         if (getRightEdge() > Constants.WINDOW_RIGHT_EDGE) {
             setRightEdge(Constants.WINDOW_RIGHT_EDGE);
@@ -23,24 +30,46 @@ public class Paddle extends Rect {
         boolean bottomLeftHit = contains(ball.getBottomLeft());
         boolean bottomRightHit = contains(ball.getBottomRight());
 
-        if (topLeftHit && bottomLeftHit) {
+        boolean ballLeftSideHit = topLeftHit && bottomLeftHit;
+        boolean ballRightSideHit = topRightHit && bottomRightHit;
+        boolean ballTopSideHit = topLeftHit && topRightHit;
+        boolean ballBottomSideHit = bottomLeftHit && bottomRightHit;
+
+        int xVel = x - lastX;
+        boolean paddleMovingLeft = (xVel < 0);
+        boolean paddleMovingRight = (xVel > 0);
+
+        int yVel = y - lastY;
+        boolean paddleMovingUp = (yVel < 0);
+        boolean paddleMovingDown = (yVel > 0);
+
+
+        // Ball's LEFT side hit while paddle moving RIGHT
+        //  -> set ball's LEFT edge to that of paddle's RIGHT
+        if (ballLeftSideHit && paddleMovingRight) {
             ball.setLeftEdge(getRightEdge());
             System.out.println("Ball's LEFT side hit!");
         }
 
-        else if (topRightHit && bottomRightHit) {
+        // Ball's RIGHT side hit while paddle moving LEFT
+        //  -> set ball's RIGHT edge to that of paddle's LEFT
+        if (ballRightSideHit && paddleMovingLeft) {
             ball.setRightEdge(getLeftEdge());
             System.out.println("Ball's RIGHT side hit!");
         }
 
-        else if (bottomLeftHit || bottomRightHit) {
-            ball.setBottomEdge(getTopEdge());
-            System.out.println("Ball's BOTTOM side hit!");
-        }
-
-        else if (topLeftHit || topRightHit) {
+        // Ball's TOP side hit while paddle moving DOWN
+        //  -> set ball's TOP edge to that of paddle's BOTTOM
+        if (ballTopSideHit && paddleMovingDown) {
             ball.setTopEdge(getBottomEdge());
             System.out.println("Ball's TOP side hit!");
+        }
+
+        // Ball's BOTTOM side hit while paddle moving UP
+        //  -> set ball's BOTTOM edge to that of paddle's TOP
+        if (ballBottomSideHit && paddleMovingUp) {
+            ball.setBottomEdge(getTopEdge());
+            System.out.println("Ball's BOTTOM side hit!");
         }
 
     }
