13f88c214c54716256d6c8cd396c86f31ec0ce13
Brian Chipman <namedtuple@gmail.com>
2015-06-22 17:32:48 -0700

    Block now extends new RectDouble which extends Rectangle2D.Double.

--- src/Block.java
+++ src/Block.java
@@ -1,11 +1,11 @@
 import java.awt.*;
 
-public class Block extends Rect {
+public class Block extends RectDouble {
 
     private Color color;
     private int hitPoints;
 
-    public Block(int xStartPos, int yStartPos, int width, int height, Color color, int hitPoints) {
+    public Block(double xStartPos, double yStartPos, double width, double height, Color color, int hitPoints) {
         super(xStartPos, yStartPos, width, height);
         this.color = color;
         this.hitPoints = hitPoints;
